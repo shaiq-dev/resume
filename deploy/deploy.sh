11f600f4689bf47924047bcce4020b7c73c502df
shaiq-dev <shaiqkar@gmail.com>
2023-08-13 03:06:26 +0530

    fix: cloudformation stack parameters

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -37,7 +37,6 @@ popd
 
 # Deploy the stack
 STACK_NAME=dresume
-STACK_PARAMETERS="BucketName=${S3_BUCKET},LambdaCodeKey=lambda/dresume.zip"
 STACK_TEMPLATE="deploy/stack.yml"
 
-aws cloudformation deploy --template-file ${STACK_TEMPLATE} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME} --no-fail-on-empty-changeset --parameters ${STACK_PARAMETERS} 
+aws cloudformation deploy --template-file ${STACK_TEMPLATE} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME} --no-fail-on-empty-changeset --parameters ParameterKey=BucketName,ParameterValue=${S3_BUCKET} ParameterKey=LambdaCodeKey,ParameterValue=lambda/dresume.zip
