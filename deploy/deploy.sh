6e654e7f2bff61007f85e2af79eb98e2f2830c42
shaiq-dev <shaiqkar@gmail.com>
2023-08-13 17:16:37 +0530

    fix: allowed origins for resume api

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,6 +9,16 @@ while [[ $# -gt 0 ]]; do
             shift # argument
             shift # value
             ;;
+        -o|--origins)
+            ALLOWED_ORIGINS="$2"
+            shift # argument
+            shift # value
+            ;;
+        -b|--bucket)
+            S3_BUCKET="$2"
+            shift # argument
+            shift # value
+            ;;
         *)
             echo "Error: Invalid command line argument $1"
             exit 255
@@ -20,7 +30,6 @@ done
 # For creating the lambda, cloudformation requires the lambbda code.
 # We will deploy the lambda code first to an S3 bucket and later
 # refrence it from the cloudformation template.
-# $S3_BUCKET - env to store the bucket name
 
 pushd .
 cd handlers
@@ -37,7 +46,7 @@ popd
 
 # Deploy the stack
 STACK_NAME=dresume
-STACK_PARAMETERS="BucketName=${S3_BUCKET} LambdaCodeKey=lambda/dresume-${VERSION}.zip"
+STACK_PARAMETERS="BucketName=${S3_BUCKET} LambdaCodeKey=lambda/dresume-${VERSION}.zip AllowedOrigins=${ALLOWED_ORIGINS}"
 STACK_TEMPLATE="deploy/stack.yml"
 
 aws cloudformation deploy --template-file ${STACK_TEMPLATE} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME} --no-fail-on-empty-changeset --parameter-overrides ${STACK_PARAMETERS}
