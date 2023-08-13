e4f5c6a6b724428ed7686994a80a58a074a3c650
shaiq-dev <shaiqkar@gmail.com>
2023-08-13 16:20:48 +0530

    added version prefix to lambda src bundle

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -24,8 +24,8 @@ done
 
 pushd .
 cd handlers
-zip -r dresume.zip index.js
-aws s3 cp ./dresume.zip s3://${S3_BUCKET}/lambda/
+zip -r dresume-${VERSION}.zip index.js
+aws s3 cp ./dresume-${VERSION}.zip s3://${S3_BUCKET}/lambda/
 popd
 
 # Deploy the build resume to S3
@@ -37,7 +37,7 @@ popd
 
 # Deploy the stack
 STACK_NAME=dresume
-STACK_PARAMETERS="BucketName=${S3_BUCKET} LambdaCodeKey=lambda/dresume.zip"
+STACK_PARAMETERS="BucketName=${S3_BUCKET} LambdaCodeKey=lambda/dresume-${VERSION}.zip"
 STACK_TEMPLATE="deploy/stack.yml"
 
 aws cloudformation deploy --template-file ${STACK_TEMPLATE} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME} --no-fail-on-empty-changeset --parameter-overrides ${STACK_PARAMETERS}
