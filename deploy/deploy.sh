31d3a735e0d8629284fd332c88a5dcf9e4d87dba
shaiq-dev <shaiqkar@gmail.com>
2024-03-10 22:51:38 +0530

    Updated lambda stack

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,16 +15,13 @@ zip -r resume-${VERSION}.zip index.js
 aws s3 cp ./resume-${VERSION}.zip s3://${BUCKET_NAME}/lambda/
 popd
 
-# # Deploy the build resume to S3
-# pushd .
-# cd src
-# mv ./resume.pdf ./resume-${VERSION}.pdf
-# aws s3 cp ./resume-${VERSION}.pdf s3://${S3_BUCKET}/resumes/
-# popd
+# Upload resume to bucket
+pushd .
+cd src
+mv ./resume.pdf ./resume-${VERSION}.pdf
+aws s3 cp ./resume-${VERSION}.pdf s3://${BUCKET_NAME}/resume/
+popd
 
-# # Deploy the stack
-# STACK_NAME=dresume
-# STACK_PARAMETERS="BucketName=${S3_BUCKET} LambdaCodeKey=lambda/dresume-${VERSION}.zip AllowedOrigins=${ALLOWED_ORIGINS}"
-# STACK_TEMPLATE="deploy/stack.yml"
-
-# aws cloudformation deploy --template-file ${STACK_TEMPLATE} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME} --no-fail-on-empty-changeset --parameter-overrides ${STACK_PARAMETERS}
+# Deploy the Lambda stack
+STACK_PARAMETERS="BucketName=${BUCKET_NAME} LambdaCodeKey=lambda/resume-${VERSION}.zip AllowedOrigins=*"
+aws cloudformation deploy --template-file "deploy/stack/lambda.yml" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --stack-name resume-lambda --no-fail-on-empty-changeset --parameter-overrides ${STACK_PARAMETERS}
