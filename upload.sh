b0e29b5aa13b70915f3981743743d70c4ab3e6ab
shaiq-dev <shaiqkar@gmail.com>
2025-08-01 12:27:21 +0530

    removed bucket name from r2 endpoint which caused duplicate folders in the bucket

--- upload.sh
+++ upload.sh
@@ -14,6 +14,7 @@ aws configure set default.region auto
 pushd .
 cd src
 mv ./resume.pdf ./resume-${VERSION}.pdf
+
 aws s3 cp ./resume-${VERSION}.pdf s3://${BUCKET}/archive/ --endpoint-url $CLOUDFLARE_R2_ENDPOINT
 
 # Override the latest version
