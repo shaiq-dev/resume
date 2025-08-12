f639ee8ba18e22a8e6f94ec27e53524add3361a5
Shaiq Kar <53471824+shaiq-dev@users.noreply.github.com>
2025-08-13 02:07:16 +0530

    added docker based build and docs (#2)

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+image="resume-builder"
+
+# Main input latex file name
+input="resume.tex"
+
+# Append version to the resume if "__VERSION__" placeholder exists
+sed -i -e "s/__VERSION__/$(git rev-parse --short=8 HEAD)/" $(pwd)/src/resume.tex
+
+docker build -t $image .
+docker run --rm -v $(pwd)/src:/data $image pdflatex $input
