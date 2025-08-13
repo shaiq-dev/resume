cbd56904a2d32f25a5c74cf6be25818319ad5995
Shaiq Kar <53471824+shaiq-dev@users.noreply.github.com>
2025-08-13 12:59:46 +0530

    fixed commit version placeholder (#4)

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ image="resume-builder"
 input="resume.tex"
 
 # Append version to the resume if "__VERSION__" placeholder exists
-sed -i -e "s/__VERSION__/$(git rev-parse --short=8 HEAD)/" $(pwd)/src/resume.tex
+sed -i -e "s/__VERSION__/$(git rev-parse --short=8 HEAD)/" $(pwd)/src/$input
 
 docker build -t $image .
 docker run --rm -v $(pwd)/src:/data $image pdflatex $input
